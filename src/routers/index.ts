c8bb878134017254f1265687e37ba8c06e5c2981
HuyenPham <huyen.phamhoang96@gmail.com>
2023-02-21 10:34:05 +0700

    new: config routers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,14 @@
 import express, { Router } from "express";
+import controller from "../controllers";
 
 const router: Router = express.Router();
 
-router.get("/");
+router.get("/", controller.onInit);
+
+router.get("/authorize", controller.renderLoginView);
+
+router.get("/signin", controller.onSignin);
+
+router.get("/token", controller.onToken);
 
 export default router;
